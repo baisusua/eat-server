851f022918c5f016271afe47aea98b52275ca4f7
kround <yuan40kang@126.com>
2017-11-27 19:57:35 +0800

    添加跨域

--- app/routes/order/order.js
+++ app/routes/order/order.js
@@ -4,7 +4,12 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../../controllers/order/order.ctrl');
-
+router.all('*',function (req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    next();
+});
 module.exports = function (app) {
     app.get('/v1/order/list', function (req, res) {
         if (req.query.date) {
--- app/routes/user/user.js
+++ app/routes/user/user.js
@@ -4,7 +4,12 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../../controllers/user/user.ctrl');
-
+router.all('*',function (req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    next();
+});
 module.exports = function (app) {
     app.get('/v1/user/list', function (req, res) {
 
